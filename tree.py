333c705d1b3b93764f64c393853195ae460aa2c4
John McAleely <john@mcaleely.com>
2024-12-01 00:01:13 +0000

    more interesting light patterns

--- tree.py
+++ tree.py
@@ -1,5 +1,6 @@
 from machine import Pin, SoftSPI
 from time import sleep
+from random import randint
 
 piLED = Pin(25, Pin.OUT, value = 1)
 
@@ -24,6 +25,11 @@ data = bytearray(header_len + numLEDs*frame_len + header_len)
 for i in range(header_len + numLEDs*frame_len, header_len + numLEDs*frame_len + header_len): data[i] = 0xff
 for i in range(header_len, header_len + numLEDs*frame_len, frame_len): data[i] = 0b11100000
 
+# an order that traverses bottom to top on each leaf clockwise.
+display_index1 = [0,  1,  2, 16, 17, 18, 15, 14, 13,  6,  5,  4, 12, 11, 10, 24, 23, 22, 19, 20, 21,  7,  8,  9, 3]
+
+# an order that traverses clockwise around the bottom, and then upwards.
+display_index  = [0, 16, 15,  6, 12, 24, 19,  7,  1, 17, 14,  5, 11, 23, 20,  8,  2, 18, 13,  4, 10, 22, 21,  9, 3]
 
 
 def setpixeloff(offset):
@@ -37,46 +43,28 @@ def setpixel(offset, brightness, r, g, b):
     data[ledFrame+1] = b
     data[ledFrame+2] = g
     data[ledFrame+3] = r
-    
-def setpattern():
-    setpixel(0, 15, 255, 0, 0)
-    setpixel(1, 15, 0, 255, 0)
-    setpixel(2, 15, 0, 0, 255)
-    setpixel(3, 2, 255, 255, 255)
-    setpixel(4, 2, 255, 255, 255)
-    setpixel(5, 2, 255, 255, 255)
-    setpixel(6, 2, 255, 0, 255)
-    setpixel(7, 2, 255, 255, 255)
-    setpixel(8, 2, 255, 255, 255)
-    setpixel(9, 2, 255, 255, 255)
-    setpixel(10, 2, 255, 255, 255)
-    setpixel(11, 2, 255, 255, 255)
-    setpixel(12, 2, 255, 255, 255)
-    setpixel(13, 2, 255, 255, 255)
-    setpixel(14, 2, 255, 255, 255)
-    setpixel(15, 2, 255, 255, 255)
-    setpixel(16, 2, 255, 0, 255)
-    setpixel(17, 2, 255, 255, 255)
-    setpixel(18, 2, 255, 255, 255)
-    setpixel(19, 2, 255, 255, 255)
-    setpixel(20, 2, 255, 255, 255)
-    setpixel(20, 2, 0, 255, 0)
-    setpixel(21, 2, 255, 255, 255)
-    setpixel(22, 2, 255, 255, 255)
-    setpixel(23, 2, 255, 255, 255)
-    setpixel(24, 2, 255, 255, 255)
+
 
 while True:
-    setpattern()
-    spi.write(data)
-
+    
     for i in range(numLEDs):
         setpixeloff(i)
-    
-    sleep(2.15)
-
     spi.write(data)
-    
-    sleep(5.15)
 
-    
+    for i in range(numLEDs - 1):
+        setpixel(display_index[i], randint(1, 4), randint(0, 255), randint(0, 255), randint(0, 255))
+        spi.write(data)
+        sleep(0.1 * randint(1, 10))
+
+    # flash the top LED
+    setpixel(display_index[numLEDs-1], 10, 255, 255, 255)
+    spi.write(data)
+    sleep(0.2)
+
+    setpixeloff(display_index[numLEDs-1])
+    spi.write(data)
+    sleep(0.2)
+
+    setpixel(display_index[numLEDs-1], 10, 255, 255, 255)
+    spi.write(data)
+    sleep(0.2)
