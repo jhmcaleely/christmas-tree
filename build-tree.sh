8a2430f71795b6987ca79dff3ab81f58469164aa
John McAleely <john@mcaleely.com>
2025-11-24 16:47:11 +0000

    run by default

--- build-tree.sh
+++ build-tree.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-HOSTYARG="../yarg-lang/bin/hostyarg"
+YARGINSTALLDIR="../yarg-lang"
+HOSTYARG="$YARGINSTALLDIR/bin/hostyarg"
+
 TARGETUF2="tree.uf2"
-
-
 if [ -e $TARGETUF2 ]
 then
     rm $TARGETUF2
 fi
 
-cp ../yarg-lang/build/yarg-lang.uf2 $TARGETUF2
+cp $YARGINSTALLDIR/build/yarg-lang.uf2 $TARGETUF2
 $HOSTYARG addfile -fs $TARGETUF2 -add randomsparkles.ya
+$HOSTYARG addfile -fs $TARGETUF2 -add main.ya
